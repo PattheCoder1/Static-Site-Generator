9763f5e4c6dd2c606484b1eb5917bb0cb14bc132
PatDevs <patirckhunter1995@gmail.com>
2025-07-27 19:54:06 -0400

    Final commit

--- src/main.py
+++ src/main.py
@@ -3,31 +3,38 @@ from enum import Enum
 
 import shutil
 import os
-import pathlib 
+import pathlib
+import sys
+
+if len(sys.argv) > 1:
+    basepath = sys.argv[1]
+else:
+    basepath = "/"
+
 
 def copy_files():
     base_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
 
     static_dir = os.path.join(base_dir, 'static')
-    public_dir = os.path.join(base_dir, 'public')
+    docs_dir = os.path.join(base_dir, 'docs')
 
-# STEP 1: Clear out the public/ directory
-    if os.path.exists(public_dir):
-        for item in os.listdir(public_dir):
-            item_path = os.path.join(public_dir, item)
+# STEP 1: Clear out the docs/ directory
+    if os.path.exists(docs_dir):
+        for item in os.listdir(docs_dir):
+            item_path = os.path.join(docs_dir, item)
             if os.path.isfile(item_path) or os.path.islink(item_path):
                 os.remove(item_path)
             elif os.path.isdir(item_path):
                 shutil.rmtree(item_path)
-        print("Cleared public/ folder.")
+        print("Cleared docs/ folder.")
     else:
-        os.makedirs(public_dir)
-        print("Created public/ folder.")
+        os.makedirs(docs_dir)
+        print("Created docs/ folder.")
 
-# STEP 2: Copy contents of static/ to public/
+# STEP 2: Copy contents of static/ to docs/
     for root, dirs, files in os.walk(static_dir):
         rel_path = os.path.relpath(root, static_dir)
-        dest_root = os.path.join(public_dir, rel_path)
+        dest_root = os.path.join(docs_dir, rel_path)
         os.makedirs(dest_root, exist_ok=True)
 
         for file in files:
@@ -44,7 +51,7 @@ def extract_title(markdown):
     raise Exception("No h1 Header in file")
 
 # Generates page from the provided contents
-def generate_page(from_path, template_path, dest_path):
+def generate_page(from_path, template_path, dest_path, basepath):
     print(f"Generating page from {from_path} to {dest_path} using {template_path}")
     with open(from_path, 'r') as file:
         from_md_content = file.read()
@@ -58,6 +65,9 @@ def generate_page(from_path, template_path, dest_path):
     # Replace empty title and contents in template file
     final_html = template_content.replace("{{ Title }}", title).replace("{{ Content }}", HTML_String)
 
+    # Replace fref and src with basepath
+    final_html = final_html.replace('href="/', f'href="{basepath}').replace('src="/', f'src="{basepath}')
+
     directory = os.path.dirname(dest_path)
     if directory:  # Only create if there's actually a directory path
         os.makedirs(directory, exist_ok=True)
@@ -65,25 +75,23 @@ def generate_page(from_path, template_path, dest_path):
     with open(dest_path, 'w') as file:
         file.write(final_html)
 
-def generate_pages_recursive(dir_path_content, template_path, dest_dir_path):
+def generate_pages_recursive(dir_path_content, template_path, dest_dir_path, basepath):
     crawl = os.listdir(dir_path_content)
     for content in crawl:
         if os.path.isfile(os.path.join(dir_path_content, content)):
             if content.endswith(".md"):
                 output_html_filename = content.replace(".md", ".html")
                 output_html_path = os.path.join(dest_dir_path,output_html_filename)
-                generate_page(os.path.join(dir_path_content,content), template_path, output_html_path)
+                generate_page(os.path.join(dir_path_content,content), template_path, output_html_path, basepath)
 
         else:
             os.makedirs(os.path.join(dest_dir_path, content), exist_ok=True)
-            generate_pages_recursive(os.path.join(dir_path_content,content), template_path, os.path.join(dest_dir_path, content))
-
-        
+            generate_pages_recursive(os.path.join(dir_path_content,content), template_path, os.path.join(dest_dir_path, content), basepath)
 
 
 def main():
     copy_files()
-    generate_pages_recursive("content", "template.html", "public")
+    generate_pages_recursive("content", "template.html", "docs", basepath)
 
 if __name__ == "__main__":
     main()
